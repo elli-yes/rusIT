91d36bc5a3dd6aa0afba0da2fcae259ed324d9fe
egor <tru.elli.yes@gmail.com>
2022-04-07 14:26:39 +0300

    buttons

--- Client/src/account/Account.jsx
+++ Client/src/account/Account.jsx
@@ -1,33 +1,37 @@
 import css from "./Account.module.css"
 import { useParams, NavLink } from "react-router-dom"
 import { Settings } from "../settings/Settings"
+import { Header } from "../shared/header/Header"
 
 export const Account = (props) => {
   const { menu } = useParams()
 
   return (
-    <div className={css.container}>
-      <div className={css.left}>
-        <div className={css.verticalMenu}>
-          <NavLink to={`/account`}>
-            <div className={menu == undefined ? css.active : ""}>Account</div>
-          </NavLink>
-          <NavLink to={`/account/login`}>
-            <div className={menu === "login" ? css.active : ""}>Login</div>
-          </NavLink>
-          <NavLink to={`/account/password`}>
-            <div className={menu === "password" ? css.active : ""}>
-              Password
-            </div>
-          </NavLink>
-          <NavLink to={`/account/token`}>
-            <div className={menu === "token" ? css.active : ""}>Token</div>
-          </NavLink>
+    <>
+      <Header />
+      <div className={css.container}>
+        <div className={css.left}>
+          <div className={css.verticalMenu}>
+            <NavLink to={`/account`}>
+              <div className={menu == undefined ? css.active : ""}>Account</div>
+            </NavLink>
+            <NavLink to={`/account/login`}>
+              <div className={menu === "login" ? css.active : ""}>Login</div>
+            </NavLink>
+            <NavLink to={`/account/password`}>
+              <div className={menu === "password" ? css.active : ""}>
+                Password
+              </div>
+            </NavLink>
+            <NavLink to={`/account/token`}>
+              <div className={menu === "token" ? css.active : ""}>Token</div>
+            </NavLink>
+          </div>
+        </div>
+        <div className={css.main}>
+          <Settings version={menu}></Settings>
         </div>
       </div>
-      <div className={css.main}>
-        <Settings version={menu}></Settings>
-      </div>
-    </div>
+    </>
   )
 }
