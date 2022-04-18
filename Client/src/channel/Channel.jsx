370b6c564d4d4ea82d598edcbf0fc30d4da63d7a
egor <tru.elli.yes@gmail.com>
2022-04-19 01:42:34 +0300

    chatik

--- Client/src/channel/Channel.jsx
+++ Client/src/channel/Channel.jsx
@@ -1,12 +1,13 @@
 import { Input } from "../shared/Input/Input"
 import { Button } from "../shared/button/Button"
 import { Header } from "../shared/header/Header"
+import css from "./Channel.module.css"
 
 export const Channel = ({ variant }) => {
   return (
     <>
       <Header />
-      <div className="container">
+      <div className={css.container}>
         <h1>Password edit</h1>
       </div>
     </>
