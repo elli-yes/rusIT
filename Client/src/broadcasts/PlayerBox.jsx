dde5cc1271d81406acae6fd6238c731575d45364
egor <tru.elli.yes@gmail.com>
2022-04-21 19:47:06 +0300

    feat:default thumb

--- Client/src/broadcasts/PlayerBox.jsx
+++ Client/src/broadcasts/PlayerBox.jsx
@@ -1,10 +1,14 @@
 import css from "./PlayerBox.module.css"
 import { NavLink } from "react-router-dom"
 
-export const PlayerBox = ({ login, title, status, ...props }) => {
+export const PlayerBox = ({ login, title, status, thumb }) => {
+  console.log(thumb)
+
   return (
     <NavLink to={`/stream/${login}`}>
-      <div className={css.playerbox}>STREAM</div>
+      <div className={css.playerbox}>
+        <img src={thumb} />
+      </div>
       <div className={css.playerboxTitle}>
         <div>{title}</div>
 
