6cd9e9d5ff6c53f696454d5a9dd2d401a1dac086
egor <tru.elli.yes@gmail.com>
2022-05-09 18:35:56 +0300

    fix:Settings form css

--- Client/src/shared/header/SearchBar.jsx
+++ Client/src/shared/header/SearchBar.jsx
@@ -5,16 +5,23 @@ import { Input } from "../Input/Input"
 
 export const SearchBar = ({ searchQuerry, setSearchQuerry }) => {
   return (
-    <div className={css.container}>
-      <Input
-        value={searchQuerry}
-        onChange={setSearchQuerry}
-        placeholder={"Search..."}
-      />
-      <Button
-        variant={"info"}
-        children={<div className={css.ico}></div>}
-      ></Button>
-    </div>
+    <form
+      action="submit"
+      onSubmit={(e) => {
+        e.preventDefault()
+      }}
+    >
+      <div className={css.container}>
+        <Input
+          value={searchQuerry}
+          onChange={setSearchQuerry}
+          placeholder={"Search..."}
+        />
+        <Button
+          variant={"info"}
+          children={<div className={css.ico}></div>}
+        ></Button>
+      </div>
+    </form>
   )
 }
